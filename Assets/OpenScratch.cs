121f9a935ae4e7038a683690980fb24dd337454f
amo-yotakenoko <amo-yotakenoko@gmail.com>
2025-06-29 15:35:44 +0900

    scratchを開くボタン

---
+++ Assets/OpenScratch.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenScratch : MonoBehaviour
+{
+    public void openScratch()
+    {
+        Application.OpenURL("https://amo-yotakenoko.github.io/VirtualRobot4_Scratch/");
+    }
+}
